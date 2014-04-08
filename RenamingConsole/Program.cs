7a98cf6579549ab542be94b73bd6264592418361
xungong91 <314568735@qq.com>
2014-04-08 19:00:45 +0800

    123

---
+++ RenamingConsole/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RenamingConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Renaming rm = new Renaming();
+            List<KeyData> keyList = new List<KeyData>(){
+            new KeyData() { key = "BaiBan_1_0_", value = "7Feng" },
+            new KeyData() { key = "BeiFeng_1_0_", value = "4Feng" },
+            new KeyData() { key = "DongFeng_1_0_", value = "1Feng" },
+            new KeyData() { key = "FaCai_1_0_", value = "6Feng" },
+            new KeyData() { key = "HongZhong_1_0_", value = "5Feng" },
+            new KeyData() { key = "NanFeng_1_0_", value = "2Feng" },
+           new KeyData() { key = "XiFeng_1_0_", value = "3Feng" }
+            };
+            List<KeyData> keylist1 = new List<KeyData>()
+            {
+                new KeyData() { key = "_1_1_", value = "" },
+            };
+
+            rm.KeyList = keylist1;
+            rm.setRenaming(@"D:\MyDocuments\GitHub\mjgame\mobile\MjMobile\Resources\sound\Girl\CaoZuo1");
+        }
+    }
+}
---
+++ RenamingConsole/Renaming.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RenamingConsole
+{
+    public class Renaming
+    {
+        private List<KeyData> keyList;
+
+        public List<KeyData> KeyList
+        {
+            get { return keyList; }
+            set { keyList = value; }
+        }
+
+        public void setRenaming(string path)
+        {
+            string filepath = path;
+            string[] filenames = Directory.GetFiles(filepath);  //获取该文件夹下面的所有文件名
+            for (int i = 0; i < filenames.Length; i++)
+            {
+                FileInfo fi = new FileInfo(filenames[i]);
+                string newFileName = string.Empty;
+                for (int j = 0; j < KeyList.Count; j++)
+                {
+                    if (filenames[i].Contains(KeyList[j].key))
+                    {
+                        newFileName = filenames[i].Replace(KeyList[j].key, KeyList[j].value);
+                        Console.WriteLine(newFileName);
+                        fi.MoveTo(newFileName);
+                        break;
+                    }
+                    
+                }
+            }
+        }
+    }
+
+    public class KeyData
+    {
+        public string key{get;set;}
+
+        public string value{get;set;}
+    }
+}
